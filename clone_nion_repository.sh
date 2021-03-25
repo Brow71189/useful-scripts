26b8eefee22e5c4b6bd42e386e29048e2a6e69a1
Brow71189 <Brow71189@gmail.com>
2021-03-25 18:20:09 +0100

    Make clone_nion_repo accept multiple repos at once.

--- clone_nion_repository.sh
+++ clone_nion_repository.sh
@@ -1,16 +1,19 @@
 #!/bin/bash        
 if [ -z "$1" ]; then 
-    echo usage: $0 directory
+    echo usage: $0 directory [directory2] [directory3] [...]
     exit
 fi
-git clone "github.com:Brow71189/$1"
-if [ $? -eq 0 ]; then
-    cd "$1"
-    git branch --set-upstream-to=origin/master
-    git remote add upstream "https://github.com/nion-software/$1"
-    echo added "https://github.com/nion-software/$1 as upstream"
-    cd ..
-else
-    git clone "https://github.com/nion-software/$1"
-fi
 
+for dir in "$@"
+do
+    git clone "github.com:Brow71189/$dir"
+    if [ $? -eq 0 ]; then
+        cd "$dir"
+        git branch --set-upstream-to=origin/master
+        git remote add upstream "https://github.com/nion-software/$dir"
+        echo added "https://github.com/nion-software/$dir as upstream"
+        cd ..
+    else
+        git clone "https://github.com/nion-software/$dir"
+    fi
+done
